ac9a5a8552b3db2f8c26d55851a71f5acaa09290
Tejas Mote <tejasmote001@gmail.com>
2023-03-03 04:29:21 +0530

    no modification

--- Project OpenGL/main.cpp
+++ Project OpenGL/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * Operating System: macOS Ventura 13.2.1 (22D68)
+ * Hardware: Apple silicon M1
+ * OpenGL version: 4.1 Metal - 83
+ * OpenGL renderer: Apple M1
+ */
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
